47ec52e43eba8933771bb9058ab2b6c3d5afba81
Cloud Assessment User <cloud_user@ip-10-0-1-11.ec2.internal>
2020-04-26 08:13:59 -0400

    Added both projects

---
+++ pgbackup/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+     long_description = f.read()
+
+setup(
+        name='pgpackup',
+        version='0.1.0',
+        author='Mughees Siddiqui',
+        author_email='muhammadmughees08@gmail.com',
+        description='A utility for backing up PostgreSQL databases',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/mughees605/pgbackup',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=['boto3'],
+        python_requires='>=3.6',
+        entry_points={
+            'console_scripts': [
+                'pgbackup=pgbackup.cli:main',
+            ],
+        }
+)
---
+++ pgbackup/src/pgbackup/cli.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser, Action
+
+class DriverAction(Action):
+     def __call__(self, parser, namespace, values, option_string=None):
+         driver, destination = values
+         namespace.driver = driver.lower()
+         namespace.destination = destination
+
+def create_parser():
+    parser = ArgumentParser(description="""
+    Back up PostgreSQL databases locally or to AWS S3.
+    """)
+
+
+    parser.add_argument("url", help="URL of database to backup")
+    parser.add_argument("--driver", "-d",
+            help="how & where to store backup",
+            nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
+            action=DriverAction,
+            required=True)
+    return parser
+
+def main():
+    import time
+    import boto3
+    from pgbackup import pgdump, storage
+
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in S3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {outfile.name}")
+        storage.local(dump.stdout, outfile)
